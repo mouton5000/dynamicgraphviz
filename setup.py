2fc2c5e72e53df151c0bee9c8dbc6cf7a7ea617d
Dimitri Watel <watel.dimitri@gmail.com>
2018-01-11 20:18:54 +0100

    Add the root package to the setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ import dynamicgraphviz
 setup(
     name='dynamicgraphviz',
     version=dynamicgraphviz.__version__,
-    packages=['dynamicgraphviz.graph', 'dynamicgraphviz.gui', 'dynamicgraphviz.gui.animations',
+    packages=['dynamicgraphviz', 'dynamicgraphviz.graph', 'dynamicgraphviz.gui', 'dynamicgraphviz.gui.animations',
               'dynamicgraphviz.helpers', 'dynamicgraphviz.exceptions'],
     url='https://github.com/mouton5000/dynamicgraphviz',
     license='MIT',
     author=dynamicgraphviz.__author__,
     author_email=dynamicgraphviz.__email__,
+    maintainer=dynamicgraphviz.__author__,
+    maintainer_email=dynamicgraphviz.__email__,
     description='A dynamic graph drawer with Gtk and Cairo',
     require=['euclid3', 'pypubsub', 'pycairo', 'pygtk']
 )
